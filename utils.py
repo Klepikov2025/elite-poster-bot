ee4e95c5f628d1459f16b3bee685971b432b13d9
Klepikov2025 <Klepikov1994@gmail.com>
2026-05-25 08:31:09 +0300

    Add files via upload

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 import re
 import pytz
-from config import VIP_PRICE_STARS
+from database import db # Подключаем базу данных напрямую сюда!
 
 def net_key_to_name(key):
     return {
@@ -9,11 +9,18 @@ def net_key_to_name(key):
     }.get(key, key)
 
 def get_referral_bonus(invites_count):
-    if invites_count <= 10:   return 0.10, int(VIP_PRICE_STARS * 0.10)
-    elif invites_count <= 30: return 0.13, int(VIP_PRICE_STARS * 0.13)
-    elif invites_count <= 50: return 0.15, int(VIP_PRICE_STARS * 0.15)
-    elif invites_count <= 100:return 0.17, int(VIP_PRICE_STARS * 0.17)
-    else:                     return 0.20, int(VIP_PRICE_STARS * 0.20)
+    # Достаем актуальную цену VIP-статуса
+    try:
+        prices_db = db['settings'].find_one({"_id": "skynet_pricing"})
+        current_vip_price = prices_db.get("vip_price", 250) if prices_db else 250
+    except Exception:
+        current_vip_price = 250
+
+    if invites_count <= 10:   return 0.10, int(current_vip_price * 0.10)
+    elif invites_count <= 30: return 0.13, int(current_vip_price * 0.13)
+    elif invites_count <= 50: return 0.15, int(current_vip_price * 0.15)
+    elif invites_count <= 100:return 0.17, int(current_vip_price * 0.17)
+    else:                     return 0.20, int(current_vip_price * 0.20)
 
 def escape_md(text):
     escape_chars = r'\_*[]()~`>#+=|{}'
